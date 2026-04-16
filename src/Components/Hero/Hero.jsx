d0413d1811f7171eef365cb7d3c54853081b5883
zunaed0314 <zunaedaman@gmail.com>
2026-04-16 10:33:18 +0600

    responsive color issue fixed

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
                     Access premium AI tools, design assets, templates, and productivity software—all in one place. Start creating faster today.
                 </p>
                 <div className='relative flex flex-col sm:flex-row gap-3'>
-                    <button className="btn bg-purple-700 rounded-full text-sm sm:text-base md:text-lg lg:text-xl py-2 sm:py-3 md:py-4 hover:bg-white hover:text-black hover:border-2 hover:border-purple-700">
+                    <button className="btn bg-purple-700 rounded-full text-sm sm:text-white md:text-lg lg:text-xl py-2 sm:py-3 md:py-4 hover:bg-white hover:text-black hover:border-2 hover:border-purple-700">
                         Explore Products
                     </button>
                     <button className='btn rounded-full border border-purple-900 py-2 sm:py-3  md:py-4 bg-white text-purple-600 font-semibold hover:bg-white hover:text-black text-sm sm:text-base md:text-lg lg:text-xl flex gap-2'>
--- src/Components/Navbar/Nav.jsx
+++ src/Components/Navbar/Nav.jsx
@@ -7,11 +7,11 @@ const Nav = ({ cartCount }) => {
             <div className="navbar-start flex  items-center gap-2 ">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden bg-purple-600 p-1">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor"> 
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> 
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="white"> 
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" stroke="white" /> 
                         </svg>
                     </div>
-                    <ul tabIndex="-1" className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow text-white">
+                    <ul tabIndex="-1" className="menu menu-sm dropdown-content bg-black rounded-box z-1 mt-3 w-52 p-2 shadow text-white">
                         <li><a>Features</a></li>
                         <li><a>Pricing</a></li>
                         <li><a>Testimonials</a></li>
